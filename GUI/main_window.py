201ed621d8b91641c2c61df752b50ac708445ce8
Tehila Naki <72945182+TehilaNaki@users.noreply.github.com>
2021-12-12 23:10:16 +0200

    add precent

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1,13 +1,13 @@
-import subprocess
 import cv2
+import duration as duration
 from guizero import *
 from tkinter import *
-from PIL import Image
-# import filedialog module
 from tkinter import filedialog
-import pyautogui as pg
-# from picamera import PiCamera
 from PIL import Image
+import tkinter.ttk as ttk
+from tkinter import *
+from tkinter.ttk import *
+
 from matplotlib.pyplot import grid
 
 '''pip3 install guizero
@@ -18,6 +18,8 @@ from matplotlib.pyplot import grid
 
 
 # Functions------------------------------------------------
+def calculate():
+    pass
 # Function for opening the file explorer window
 def browseFiles():
     filename = filedialog.askopenfilename(initialdir="/",
@@ -48,18 +50,21 @@ def capture_img():
 # APP-----------------------------------------------------
 NAME = 'AI_PROJECT'
 MESSAGE = 'please insert image'
-app = App(title=NAME,layout='grid')
+app = App(title=NAME, layout='grid')
 
 # Widgets---------------------------------------------------
- #message = Text(app, text=MESSAGE,grid=[8,0])
- #message.text_size = 40
+#message = Text(app, text=MESSAGE)
+#message.text_size = 40
 app.bg = 'pink'
-# img=Picture(app,image='img1.png')
-up = PushButton(app, upload_img, text='Upload',grid=[0,0,40,2])
+up = PushButton(app, upload_img, text='Upload', grid=[0, 0, 40, 2])
 up.bg = 'red'
-take = PushButton(app, capture_img, text='Take a picture',grid=[40,0,30,2])
+take = PushButton(app, capture_img, text='Take a picture', grid=[40, 0, 30, 2])
 take.bg = 'red'
-pic = Drawing(app, grid=[1,4,70,70])
-viewer = Picture(app,grid=[1,4,70,70])
+rep = PushButton(app, calculate, text='Report', grid=[70, 0, 40, 2])
+rep.bg = 'red'
+pic = Drawing(app, grid=[1, 4, 70, 70])
+viewer = Picture(app, grid=[1, 4, 70, 70])
+duration=Slider(app,start=0,end=100,grid=[50,50])
+duration.bg = 'green'
 # Display---------------------------------------------------
 app.display()
--- GUI/precent.py
+++ GUI/precent.py
@@ -1,4 +1,4 @@
-# Python program to create
+'''# Python program to create
 # a file explorer in Tkinter
 
 # import all components
@@ -60,4 +60,42 @@ button_explore.grid(column=1, row=2)
 button_exit.grid(column=1, row=3)
 
 # Let the window wait for any events
-window.mainloop()
+window.mainloop()'''
+import time
+import tkinter as tk
+import tkinter.ttk as ttk
+
+tuple_1 = tuple(range(1, 25))
+
+def progress_bar_func(style, progress_bar, sequence):
+    root.after(500, update_progress_bar, style, progress_bar, 1, len(sequence))
+
+def update_progress_bar(style, progress_bar, num, limit):
+    if num <= limit:
+        percentage = round(num/limit * 100)  # Calculate percentage.
+        progress_bar.config(value=num)
+        style.configure('text.Horizontal.TProgressbar', text='{:g} %'.format(percentage))
+        num += 1
+        root.after(500, update_progress_bar, style, progress_bar, num, limit)
+
+root = tk.Tk()
+root.geometry("300x300")
+
+style = ttk.Style(root)
+style.layout('text.Horizontal.TProgressbar',
+             [('Horizontal.Progressbar.trough',
+               {'children': [('Horizontal.Progressbar.pbar',
+                              {'side': 'left', 'sticky': 'ns'})],
+                'sticky': 'nswe'}),
+              ('Horizontal.Progressbar.label', {'sticky': ''})])
+style.configure('text.Horizontal.TProgressbar', text='0 %')
+
+progress_bar = ttk.Progressbar(root, style='text.Horizontal.TProgressbar', length=200,
+                               maximum=len(tuple_1), value=0)
+progress_bar.pack()
+
+progress_button = tk.Button(root, text="start",
+                            command=lambda: progress_bar_func(style, progress_bar, tuple_1))
+progress_button.pack()
+
+root.mainloop()
