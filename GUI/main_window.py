280c7132e2f21ddcfccbbcd86a6b913899bb102a
Tehila Naki <72945182+TehilaNaki@users.noreply.github.com>
2022-01-03 23:18:39 +0200

    add emotion

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1,13 +1,11 @@
 import cv2
 import duration as duration
 from guizero import *
+from guizero import Text
 from tkinter import *
-#from tkinter import filedialog
 from PIL import Image
-import tkinter.ttk as ttk
-from tkinter import *
-from tkinter.ttk import *
-from PIL import Image
+from guizero import App, Text
+from matplotlib._text_layout import layout
 
 from matplotlib.pyplot import grid
 
@@ -17,11 +15,31 @@ pip3 install guizero[images]
 set READTHEDOCS=True
 pip install picamera
 '''
-
+DATA_PATH = 'POSE_DETECTION/images'
+EMOTIONS_LIST= ['positive_interaction', 'discomfort', 'insecure', 'anger', 'happines', 'excitement', 'confidence', 'confusion', 'reliability']
 
 # Functions------------------------------------------------
-def calculate():
+def recommand(emo):
     pass
+
+
+def calculate(emo_res, emo_list):
+    button_list = {}
+    emo_box = Box(app, grid=[5, 0],align='right')
+
+    for emo in emo_list:
+        #im = Image.open(emo + '.jpg')
+        im = Image.open('angry'+ '.jpg')
+        im = im.resize((50, 50))
+        box = Box(emo_box, align='top')
+        PushButton(box, command=recommand(emo), align='right', image=im)
+        txt=Text(box,align='right',text=emo,height=1,width=20)
+        button_list[emo] = box
+        if emo in emo_res:
+            button_list[emo].bg = 'green'
+        else:
+            button_list[emo].bg = 'yellow'
+    #return button_list
 
 
 # Function for opening the file explorer window
@@ -40,8 +58,7 @@ def upload_img():
     filename = browseFiles()
     # open method used to open different extension image file
     im = Image.open(filename)
-    im = im.resize((20, 20))
-    # This method will show image in any image viewer
+    im = im.resize((400, 400))
     im.save("upload_images/upload_img1.jpg")
     viewer.image = r'upload_images/upload_img1.jpg'
 
@@ -50,8 +67,8 @@ def capture_img():
     cam = cv2.VideoCapture(0)
     frame = cam.read()[1]
     cv2.imwrite(r'compute_images/img.jpg', frame)
-    im=Image.open(r'compute_images/img.jpg')
-    im = im.resize((500, 500))
+    im = Image.open(r'compute_images/img.jpg')
+    im = im.resize((500, 20))
     im.save(r'compute_images/img.jpg')
     viewer.image = r'compute_images/img.jpg'
 
@@ -59,23 +76,23 @@ def capture_img():
 # APP-----------------------------------------------------
 NAME = 'AI_PROJECT'
 MESSAGE = 'please insert image'
-app = App(title=NAME,layout='grid')
+app = App(title=NAME, layout='grid', height=650, width=650)
 
 # Widgets---------------------------------------------------
-# message = Text(app, text=MESSAGE)
+#message = Text(app, text=MESSAGE)
 # message.text_size = 40
-app.bg = 'pink'
-up = PushButton(app, upload_img, text='Upload',grid=[0,1],align='left')
+b = Box(app, grid=[0,1, 3, 1])
+app.bg = 'lightblue'
+up = PushButton(b, command=upload_img, text='Upload', align='left')
 up.bg = 'red'
-take = PushButton(app, capture_img, text='Take a picture',grid=[1,1],align='left')
+take = PushButton(b, command=capture_img, text='Take a picture', align='left')
 take.bg = 'red'
-rep = PushButton(app, calculate, text='Report', grid=[2,1],align='left')
+rep = PushButton(b, command=calculate(['anger'], EMOTIONS_LIST), text='Report', align='left')
 rep.bg = 'red'
-viewer = Picture(app,grid=[3,0],align='right')
-viewer.img=r'compute_images/th.jpg'
+viewer = Picture(app,image='th.jpg',grid=[1, 0])
+#name_img = TextBox(app, grid=[0, 1])
+#text = Text(app, text="recommand",grid=[0,30])
 
-# duration=Slider(app,start=0,end=100,grid=[50,50])
-# duration.bg = 'green'
 
 # Display---------------------------------------------------
 app.display()
