4a3f516bf4cc528d3a2e4b64553dc916370bbc0c
Tehila Naki <72945182+TehilaNaki@users.noreply.github.com>
2022-01-16 23:08:13 +0200

    fix bug of format file,design,add recommendation

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1,23 +1,24 @@
 from datetime import datetime
 from tkinter import *
-from tkinter import filedialog
-
 import cv2
 from PIL import Image
 from guizero import *
-
 from POSE_DETECTION.pose_classifer import PoseClassifier
+import recommendation as r
 
 '''
+#TODO
 pip3 install guizero
 pip3 install guizero[images]
 set READTHEDOCS=True
 pip install picamera
 '''
+
 DATA_PATH = 'POSE_DETECTION/images'
 POSITIVE_EMO = ['positive_interaction', 'authoritative', 'appriciation', 'confidence', 'calm', 'excitement', 'happines',
                 'reliability']
-NEGATIVE_EMO = ['insecure', 'fear', 'discomfort', 'anger', 'shyness', 'confusion', 'stubbornness', 'interest in surroundings']
+NEGATIVE_EMO = ['insecure', 'fear', 'discomfort', 'anger', 'shyness', 'confusion', 'stubbornness',
+                'interest in surroundings']
 
 
 # Functions------------------------------------------------
@@ -25,56 +26,6 @@ def classify_pose(path):
     pose_classifier = PoseClassifier(r'..\POSE_DETECTION\model')
     poses = pose_classifier.classify_pose(path)
     return poses
-
-
-def recommand(emo):
-    pass
-
-
-def visual(emo_res, pos, neg):
-    button_list = {}
-    pos_box = Box(app, grid=[1, 0, 1, 2], align='left')
-    neg_box = Box(app, grid=[2, 0, 1, 2], align='left')
-    '''if emo_res == None:
-        rec_txt.destroy()
-        rec_txt=Text(app,text='THERE NO classify_pose',grid=[0,2,3,1],height=5, width=110,bg='pink',align='center')'''
-    for emo in pos:
-        im = Image.open('emotion_icons/' + emo + '.jpg')
-        im = im.resize((50, 50))
-        box = Box(pos_box, align='top')
-        PushButton(box, command=recommand(emo), align='right', image=im)
-        Emo = emo[0].upper() + emo[1:]
-        txt = Text(box, align='right', text=Emo, height=4, width=21)
-        button_list[emo] = box
-        if emo_res != None and emo in emo_res:
-            button_list[emo].bg = 'green'
-        else:
-            button_list[emo].bg = 'yellow'
-    for emo in neg:
-        im = Image.open('emotion_icons/' + emo + '.jpg')
-        im = im.resize((50, 50))
-        box = Box(neg_box, align='top')
-        PushButton(box, command=recommand(emo), align='right', image=im)
-        Emo = emo[0].upper() + emo[1:]
-        Text(box, align='right', text=Emo, height=4, width=21)
-        button_list[emo] = box
-        if emo_res != None and emo in emo_res:
-            button_list[emo].bg = 'green'
-        else:
-            button_list[emo].bg = 'yellow'
-    # return button_list
-
-
-# Function for opening the file explorer window
-def browseFiles():
-    filename = filedialog.askopenfilename(initialdir="/Pictures",
-                                          title="Select a File",
-                                          filetypes=(("IMAGE_FILES",
-                                                      "*.jpg*"),
-                                                     ("all files",
-                                                      "*.*")))
-
-    return filename
 
 
 def get_new_name():
@@ -96,8 +47,12 @@ def view_img(img_path):
 def upload_img():
     filename = browseFiles()
     im = Image.open(filename)
-    viewer.image = im
+    if im.format != 'jpg':
+        im = im.convert("RGB")
+    if im.width > im.height:
+        im = im.rotate(180)
     filename = get_new_name()
+    viewer.image = im
     im.save(filename)
     view_img(filename)
 
@@ -112,23 +67,81 @@ def capture_img():
     view_img(filename)
 
 
+def recommand(*emo):
+    rec = Window(app, title='Analysis', width=740, height=700)
+    rec.bg = 'beige'
+    rec.font = 'Calibri'
+    rec._text_size = 15
+    txt_rec = Text(rec, align='center')
+    txt_rec.value = r.rec[''.join(emo)]
+    txt_rec.align = 'left'
+    rec.show()
+
+
+def visual(emo_res, pos, neg):
+    button_list = {}
+    pos_box = Box(app, grid=[1, 0, 1, 2], align='left')
+    neg_box = Box(app, grid=[2, 0, 1, 2], align='left')
+    for emo in pos:
+        im = Image.open('emotion_icons/' + emo + '.jpg')
+        im = im.resize((50, 50))
+        box = Box(pos_box, align='top')
+        PushButton(box, command=recommand, args=emo, align='right', image=im)
+        Emo = emo[0].upper() + emo[1:]
+        Text(box, align='right', text=Emo, height=4, width=21)
+        button_list[emo] = box
+        if emo_res is not None and emo in emo_res:
+            button_list[emo].bg = 'LightCyan2'
+        else:
+            button_list[emo].bg = 'ivory2'
+    for emo in neg:
+        im = Image.open('emotion_icons/' + emo + '.jpg')
+        im = im.resize((50, 50))
+        box = Box(neg_box, align='top')
+        PushButton(box, command=recommand, args=emo, align='right', image=im)
+        Emo = emo[0].upper() + emo[1:]
+        Text(box, align='right', text=Emo, height=4, width=21)
+        button_list[emo] = box
+        if emo_res is not None and emo in emo_res:
+            button_list[emo].bg = 'LightCyan2'
+        else:
+            button_list[emo].bg = 'ivory2'
+
+
+# Function for opening the file explorer window
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir="/Pictures",
+                                          title="Select a File",
+                                          filetypes=(("IMAGE_FILES",
+                                                      "*.jpg*"),
+                                                     ("all files",
+                                                      "*.*")))
+
+    return filename
+
+
 # APP-----------------------------------------------------
 NAME = 'AI_PROJECT'
-MESSAGE = 'please insert image'
-app = App(title=NAME, layout='grid', height=728, width=998)
-app.bg = 'lightblue'
-# Widgets---------------------------------------------------
+MESSAGE = 'Please insert image'
+BG_IMAGE = 'emotion_icons/mad.jpg'
+app = App(title=NAME, layout='grid', height=732, width=985)
+app.bg = 'pink1'
+app.font = 'Calibri'
+# APP Widgets---------------------------------------------------
 b = Box(app, grid=[0, 1])
 
 up = PushButton(b, command=upload_img, text='Upload', align='left', height=5, width=30)
-up.bg = 'blue'
+up.bg = 'pink1'
 
 take = PushButton(b, command=capture_img, text='Take a picture', align='left', height=5, width=30)
-take.bg = 'blue'
+take.bg = 'pink1'
 
-viewer = Picture(app, image='emotion_icons/mad.jpg', grid=[0, 0], height=500, width=480)
+viewer = Picture(app, image=BG_IMAGE, grid=[0, 0], height=500, width=480)
+view_img(BG_IMAGE)
 
-rec_txt = Text(app, grid=[0, 2, 3, 1], height=5, width=110, bg='pink', align='top', text='recommand')
+Text(app, grid=[0, 2, 3, 1], height=4, width=120, bg='pink1', align='top',
+     text='For an explanation of image analysis and additional recommendations for a specific classification:\n Click '
+          'the icon button next to the classification.')
 
 # Display---------------------------------------------------
 app.display()
---
+++ GUI/recommendation.py
@@ -0,0 +1,53 @@
+rec = {
+    'positive_interaction': 'Maintain good eye contact\n'
+                            ' Maintaining good eye contact shows you are engaged and actively listening to what'
+                            '\n someone is telling you.'
+                            '\n However, if you stare too intensely, it can have the opposite effect by making'
+                            '\n the other person feel too targeted.'
+                            '\n Assess their body language in response to determine if they feel comfortable. '
+                            '\n \n Head nod '
+                            '\n In addition to showing a speaker that you agree with their message, '
+                            '\n nodding your head shows others that you are listening to what they have to say.'
+                            '\n When you pair a head nod with a smile, you’re more likely to engage them with '
+                            '\n inclusive enthusiasm. '
+                            '\n Leaning in while speaking (and listening) The distance you keep between others '
+                            '\n is also a nonverbal cue. '
+                            '\n While you never want to impose on someone’s personal space, leaning in and '
+                            '\n facing their direction when communicating indicates'
+                            '\n that you are comfortable with them and interested in the exchange. '
+                            '\n \n Minimal facial expressions '
+                            '\n If you are trying to avoid showing any type of facial expression, it may be '
+                            '\n perceived as you having something to hide.'
+                            '\n Limited facial expression can also be interpreted as an intimidation tactic '
+                            '\n or disinterest in the conversation.'
+                            '\n Similarly, if you keep your eyes on the floor or maintain little to no eye '
+                            '\n contact with someone, \n it can indicate that you are uncomfortable, shy or even '
+                            '\ndishonest. ',
+    'authoritative': 'Hands '
+                     '\n Talking with your hands is an easy way to incorporate gestures into your conversation '
+                     '\n but be careful not to make it a dance party.'
+                     '\n Emphasizing words with your hands can lead you to appear more credible and assured.'
+                     '\n \n arms held behind body with hands clasped'
+                     '\n As demonstrated by members of the royal family, armed forces officers, teachers, policemen.. '
+                     '\n \n finger pointing (in the air)\n  Pointing in the air is generally used to add emphasis, '
+                     '\n by a person feeling in authority or power. '
+                     '\n \n head tilted downward \n Head tilted downwards towards a person is commonly a signal of '
+                     '\n criticism or reprimand or disapproval, usually from a position of authority. '
+                     '\n \n palm(s) down \n Where the lower arm moves across the body with palm down this is generally'
+                     '\n defiance or firm disagreement. '
+}
+''''
+    'appriciation',
+    'confidence',
+    'calm',
+    'excitement',
+    'happines',
+    'reliability',
+    'insecure',
+    'fear',
+    'discomfort',
+    'anger',
+    'shyness',
+    'confusion',
+    'stubbornness',
+    'interest in surroundings'''
